09cd8be3d2ed4030332a2863b3e8a5ebe2de5fa5
Cloud User <centos@ip-172-31-26-250.eu-west-2.compute.internal>
2020-03-13 01:16:01 +0000

    add test file

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -43,9 +43,27 @@ output "Centos_AMI_ID" {
   value       = "${data.aws_ami.centos.id}"
 }
 
+resource "aws_key_pair" "provisioner" {
+  key_name   = "provisioner-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
+  key_name = "${aws_key_pair.provisioner.key_name}"
+
+
+  # Copies the file as the root user using SSH
+provisioner "file" {
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    host     = "${self.public_ip}"
+     }
+  source      = "test"
+  destination = "/tmp/test"
+  }
 
   tags = {
     Name = "HelloWorld"
